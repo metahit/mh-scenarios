d968edd60e808d5998d5393c089e9b0c107ab32a
Ali Abbas <m.aliabbas@gmail.com>
2021-10-13 12:48:35 +0100

    Rename var name

--- 1.4_WalkIncreaseScenario_CityReg.R
+++ 1.4_WalkIncreaseScenario_CityReg.R
@@ -7,7 +7,7 @@ memory.limit(size=1000000)
 ## STEP 0: INPUT DATA AND PARAMETERS 
 
 # Scenario name
-scenname <- 'wlk_10p_inc_scen'
+scen_name <- 'wlk_10p_inc_scen'
 
 # Load data
 # Define LA list
@@ -231,9 +231,9 @@ for(j in 1:length(lad14$lad14cd)){
   
   # Save dataset
   if(lafull==1) {
-    saveRDS(sp_ind, file.path("../mh-execute/inputs/scenarios", scenname, paste0("SPind_", lahome, ".Rds")),version=2)
+    saveRDS(sp_ind, file.path("../mh-execute/inputs/scenarios", scen_name, paste0("SPind_", lahome, ".Rds")),version=2)
   } else {
-    saveRDS(sp_ind, file.path("../mh-execute/inputs/scenarios-mini", scenname, paste0("SPind_", lahome, ".Rds")),version=2)
+    saveRDS(sp_ind, file.path("../mh-execute/inputs/scenarios-mini", scen_name, paste0("SPind_", lahome, ".Rds")),version=2)
   }
   
   print(paste0("Scenario run for home LA ",lahome, " (", j , ") at ",Sys.time()))
